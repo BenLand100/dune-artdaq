69ad1ddaaead91a91d6fd9f3e3df66f55db82d74
Kurt Biery <biery@fnal.gov>
2014-05-02 16:18:24 -0500

    Changed raw data file prefix from artdaqdemo to lbne.

--- tools/generateEventBuilderMain.rb
+++ tools/generateEventBuilderMain.rb
@@ -105,7 +105,7 @@ end
 
 
 currentTime = Time.now
-fileName = "artdaqdemo_eb%02d_" % ebIndex
+fileName = "lbne_eb%02d_" % ebIndex
 fileName += "r%06r_sr%02s_%to"
 fileName += ".root"
 outputFile = File.join(dataDir, fileName)
