d5d37c8c6b3d4c5a5eadfd65bb235e7350119f6d
LBNE DAQ <lbnedaq@lbne35t-gateway01.fnal.gov>
2014-08-19 13:56:05 -0500

    Improved formatting to LbneArtdaqPrinVersionInfo output + locally disabled unused-parameter compiler error to remove unnecessary body of the required analyze() function override

--- lbne-artdaq/ArtModules/LbneArtdaqPrintVersionInfo_module.cc
+++ lbne-artdaq/ArtModules/LbneArtdaqPrintVersionInfo_module.cc
@@ -7,6 +7,24 @@
 // from cetpkgsupport v1_05_02.
 ////////////////////////////////////////////////////////////////////////
 
+
+// JCF, 8/19/14
+
+// As the following function:
+
+// void analyze(art::Event const & e) 
+
+// in LbneArtdaqPrintVersionInfo's base class, art::EDAnalyzer, needs
+// to be overridden, but this module only analyzes run-by-run info
+// rather than event-by-event info, there's no point in adding an
+// implementation body to the function. Thus, for this module we
+// disable the error condition usually called by an unused function
+// parameter (here, the reference-to-art::Event)
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
@@ -30,9 +48,6 @@ public:
   virtual ~LbneArtdaqPrintVersionInfo();
 
   void analyze(art::Event const & e) override { 
-    if (e.run() == 99999999) {
-      std::cout << "JCF, 8/16/16 -- the only reason the analyze() function in LbneArtdaqPrintVersionInfo has a body is because the compiler complains if the art::Event argument goes unused" << std::endl;
-    }
   }
 
   virtual void beginRun(art::Run const& r);
@@ -73,24 +88,32 @@ void lbne::LbneArtdaqPrintVersionInfo::beginRun(art::Run const& run)
   run.getByLabel(lbne_raw_data_module_label_, lbne_raw_data_instance_label_, raw);
 
   if (raw.isValid()) {
+    std::cout << std::endl;
     std::cout << "lbne-raw-data package version: " << raw->getPackageVersion() << std::endl;
     std::cout << "lbne-raw-data package build time: " << raw->getBuildTimestamp() << std::endl;
-
+    std::cout << std::endl;
   } else {
-    std::cout << "Run " << run.run() << " appears to have no info on lbne-raw-data build time / version number" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Warning in LbneArtdaqPrintVersionInfo module: Run " << run.run() << " appears to have no info on lbne-raw-data build time / version number" << std::endl;
+    std::cerr << std::endl;
   }
 
 
   run.getByLabel(lbne_artdaq_module_label_, lbne_artdaq_instance_label_, raw);
 
   if (raw.isValid()) {
+    std::cout << std::endl;
     std::cout << "lbne-artdaq package version: " << raw->getPackageVersion() << std::endl;
     std::cout << "lbne-artdaq package build time: " << raw->getBuildTimestamp() << std::endl;
-
+    std::cout << std::endl;
   } else {
-    std::cout << "Run " << run.run() << " appears to have no info on lbne-artdaq build time / version number" << std::endl;
+    std::cerr << std::endl;
+    std::cerr << "Warning in LbneArtdaqPrintVersionInfo module: Run " << run.run() << " appears to have no info on lbne-artdaq build time / version number" << std::endl;
+    std::cerr << std::endl;
   }
 
 }
+
+#pragma GCC diagnostic pop
 
 DEFINE_ART_MODULE(lbne::LbneArtdaqPrintVersionInfo)
