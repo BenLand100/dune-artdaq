a4498126d64de29b52212ddcae987cd341d1ee57
Matthew Tamsett <m.tamsett@sussex.ac.uk>
2014-12-15 10:04:07 -0600

    Add in example SSP trigger module which simply cuts on a configurable number of triggers.

--- tools/generateTrigger.rb
+++ tools/generateTrigger.rb
@@ -4,9 +4,14 @@ def generateTrigger( )
 triggerConfig = String.new( "\
   filters: {
     exampletrigger:{
-      module_type: PreScaleTrigger
-      PreScale:0
-      UseRndmPreScale: true
+      # module_type: PreScaleTrigger
+      # PreScale:0
+      # UseRndmPreScale: true
+      module_type: SSPTrigger
+      SspModuleLabel: daq
+      CutOnNTriggers: true
+      MinNTriggers: 2
+      Verbose: true
     }
   }
 
