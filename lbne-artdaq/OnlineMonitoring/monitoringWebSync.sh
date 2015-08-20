5a97feedbacd784d3558eebca1bf8bb5b0883dbb
kaikai581 <kaikai5816787@gmail.com>
2015-08-20 15:10:32 -0500

    Updated monitoring transfer script which used the lbnedaq_cron principal to gain permissions, rather than my personal one

--- lbne-artdaq/OnlineMonitoring/monitoringWebSync.sh
+++ lbne-artdaq/OnlineMonitoring/monitoringWebSync.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-KEYTAB=/var/adm/krb5/`/usr/krb5/bin/kcron -f`
+#KEYTAB=/var/adm/krb5/`/usr/krb5/bin/kcron -f`
+KEYTAB=/var/adm/krb5/lbnedaq_cron.keytab
 KEYUSE=`/usr/krb5/bin/klist -k ${KEYTAB} | grep FNAL.GOV | head -1 | cut -c 5- | cut -f 1 -d /`
-/usr/krb5/bin/kinit -5 -A  -kt ${KEYTAB} ${KEYUSE}/cron/`hostname`@FNAL.GOV
+/usr/krb5/bin/kinit -5 -A  -kt ${KEYTAB} ${KEYUSE}/dune/`hostname`@FNAL.GOV
 
-host=wallbank@lbnegpvm01.fnal.gov
+phost=lbnedaq@lbnegpvm01.fnal.gov
 
 cd /data/lbnedaq/monitoring
 
 for directory in `find . -type d`
 do
     if [ -e ${directory}/run ]; then
+
+	echo "In directory ${directory}"
 
 	# Get run/subrun
 	read -r runsubrun < ${directory}/run
@@ -22,10 +25,10 @@ do
 	date=`date +"%A %B %_d %Y, %H:%M (%Z)"`
 
 	# Copy the directory to the server
-	scp -r $directory ${host}:/web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring
+	scp -r $directory ${phost}:/web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring
 
 	# Add this new directory to index.html so it's displayed on web
-	ssh wallbank@lbnegpvm01.fnal.gov "sed -i '5i</br><a href=\"Run${run}Subrun${subrun}\">Run ${run}, Subrun ${subrun} (uploaded ${date})</a>' /web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring/index.html"
+	ssh ${phost} "sed -i '5i</br><a href=\"Run${run}Subrun${subrun}\">Run ${run}, Subrun ${subrun} (uploaded ${date})</a>' /web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring/index.html"
 
 	# Tidy up
 	# 18Jun -- copy files into the monitoring directory to keep only the most recent run
