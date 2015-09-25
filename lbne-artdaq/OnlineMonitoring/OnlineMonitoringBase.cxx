c99b9d58aafb99d9ca1a8a3b1e942840e68df5b2
kaikai581 <kaikai5816787@gmail.com>
2015-09-25 15:37:07 -0500

    working Pedestal monitor

---
+++ lbne-artdaq/OnlineMonitoring/OnlineMonitoringBase.cxx
@@ -0,0 +1,50 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Online Monitoring namespace
+// Author: Mike Wallbank, m.wallbank@sheffield.ac.uk (September 2015)
+//
+// Contains global variables used by classes in the monitoring namespace.
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef OnlineMonitoringNamespace
+#define OnlineMonitoringNamespace
+
+#include "TString.h"
+
+namespace OnlineMonitoring {
+
+  class OnlineMonitoring;
+  class OnlinePedestal;
+  class RCEFormatter;
+  class SSPFormatter;
+  class PTBFormatter;
+  class MonitoringData;
+  class MonitoringPedestal;
+  class EventDisplay;
+  class ChannelMap;
+  struct Channel;
+
+  const unsigned int NRCEChannels    = 512;
+  const unsigned int NRCEMillislices = 4;
+  const unsigned int NSSPChannels    = 96;
+  const std::vector<int> DebugChannels = {260, 278, 289, 290};
+
+  // Paths
+  // Note -- on the gateway machine the default paths here refer to directories constantly
+  // being scanned by cron jobs to upload new monitoring to the web.
+  // If wanting to test and develop code, please change the paths!
+  const TString DataDirName   = "/data/lbnedaq/data/";
+  const TString HistSavePath  = "/data/lbnedaq/monitoring/";
+  const TString EVDSavePath   = "/data/lbnedaq/eventDisplay/";
+  const TString ImageType     = ".png";
+  const TString PathDelimiter = "_";
+
+  const bool _verbose = false;
+
+  namespace TypeSizes {
+    static int const CounterWordSize = 128;
+    static int const TriggerWordSize = 32;
+  }
+
+}
+
+#endif
