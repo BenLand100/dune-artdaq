f7e17f0afc4122efa8255003125bf68d473191b3
LBNE DAQ <lbnedaq@lbne35t-gateway02.fnal.gov>
2015-10-29 17:47:49 -0500

    MW: updated script for cron job copy

--- lbne-artdaq/OnlineMonitoring/monitoringWebSync.sh
+++ lbne-artdaq/OnlineMonitoring/monitoringWebSync.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#KEYTAB=/var/adm/krb5/`/usr/krb5/bin/kcron -f`
-KEYTAB=/var/adm/krb5/lbnedaq_cron.keytab
+# Get a ticket!
+KEYTAB=/var/adm/krb5/lbnedaq.keytab
 KEYUSE=`/usr/krb5/bin/klist -k ${KEYTAB} | grep FNAL.GOV | head -1 | cut -c 5- | cut -f 1 -d /`
 /usr/krb5/bin/kinit -5 -A  -kt ${KEYTAB} ${KEYUSE}/dune/`hostname`@FNAL.GOV
-
-phost=lbnedaq@lbnegpvm01.fnal.gov
 
 # Look at the monitoring plots
 cd /data/lbnedaq/monitoring
@@ -24,10 +22,13 @@ do
 	date=`date +"%A %B %_d %Y, %H:%M (%Z)"`
 
 	# Copy the directory to the server
-	scp -r $directory ${phost}:/web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring
+	cp -r $directory /web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring
 
 	# Add this new directory to index.html so it's displayed on web
-	ssh ${phost} "sed -i '5i</br><a href=\"Run${run}Subrun${subrun}\">Run ${run}, Subrun ${subrun} (uploaded ${date})</a>' /web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring/index.html"
+	RECENTRUN=`sed '7q;d' /web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring/index.html`
+	if [[ ! ${RECENTRUN} == "</br><a href=\"Run${run}Subrun${subrun}"* ]]; then
+	    sed -i "7i</br><a href=\"Run${run}Subrun${subrun}\">Run ${run}, Subrun ${subrun} (updated ${date})</a>" /web/sites/lbne-dqm.fnal.gov/htdocs/OnlineMonitoring/index.html
+	fi
 
 	# Tidy up
 	# 18Jun -- copy files into the monitoring directory to keep only the most recent run
@@ -43,9 +44,7 @@ cd /data/lbnedaq/eventDisplay
 
 if [ -e event ]; then
 
-    scp ${phost}:/web/sites/lbne-dqm.fnal.gov/htdocs/EventDisplay
+    cp evd.png /web/sites/lbne-dqm.fnal.gov/htdocs/EventDisplay/
     rm -f event
 
 fi
-
-/usr/krb5/bin/kdestroy
