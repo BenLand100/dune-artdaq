11ef9c59d500326929cfedc06c8ba2c4e6758318
Enrico Gamberini <enrico.gamberini@cern.ch>
2017-09-14 16:23:23 +0200

    JCF: modified WIBReader to conform to standard dune-artdaq package practices, i.e., use of smart pointers and use of DAQLogger for output

--- dune-artdaq/Generators/WIBReader_generator.cc
+++ dune-artdaq/Generators/WIBReader_generator.cc
@@ -1,7 +1,10 @@
 #include "WIBReader.hh"
 #include "artdaq/Application/GeneratorMacros.hh"
+#include "dune-artdaq/DAQLogger/DAQLogger.hh"
 
 #include <iostream>
+#include <memory>
+#include <iomanip>
 
 namespace wibdaq {
 
@@ -10,9 +13,20 @@ WIBReader::WIBReader(fhicl::ParameterSet const& ps) :
     CommandableFragmentGenerator(ps) {
 
   std::string wib_address = ps.get<std::string>("WIB.address");
-  printf("Connecting to WIB at %s\n", wib_address.c_str());
+  dune::DAQLogger::LogInfo("WIBReader") << "Connecting to WIB at " <<  wib_address;
 
-  wib = new WIB(wib_address, "WIB.adt", "FEMB.adt");
+  // JCF, Sep-14-2017
+
+  // It's safer to use one of C++11's "smart" pointers than to use a
+  // raw pointer, as objects pointed to by smart pointers will be
+  // automatically cleaned up when the smart pointer goes out of
+  // scope, without requiring the programmer to cleanup with the
+  // "delete" keyword. However, be aware that here, deallocation will
+  // occur at the end of the constructor. To make the WIB instance
+  // persistent through the lifetime of WIBReader, make it a WIBReader
+  // member.
+
+  auto wib = std::make_unique<WIB>( wib_address, "WIB.adt", "FEMB.adt" );
 
   fhicl::ParameterSet wib_config = ps.get<fhicl::ParameterSet>("WIB.config");
   // Read values from FHiCL document and feed them to WIB library
@@ -20,10 +34,10 @@ WIBReader::WIBReader(fhicl::ParameterSet const& ps) :
     if (wib_config.is_key_to_atom(key)) {
       uint32_t value = wib_config.get<long int>(key);
       wib->Write(key, value);
-      printf("Set %s to 0x%08x\n", key.c_str(), value);
+      dune::DAQLogger::LogInfo("WIBReader") << "Set " << key << " to 0x" << std::hex << std::setw(8) << std::setfill('0') << value << std::dec;
     }
   }
-  printf("Configured WIB\n");
+  dune::DAQLogger::LogInfo("WIBReader") << "Configured WIB";
 }
 
 // "shutdown" transition
