affbe836bd2ed5c19c3f513f7e35f94bb4eac174
Justin Hugon <5174443+jhugon@users.noreply.github.com>
2017-10-10 16:21:01 +0200

    WIBReader: more fcl, exceptions, FELIX
      fcl params for everything
      exceptions if unexpected RCE or FELIX fw or fw number
      maybe work with FELIX if fcl setup right?

--- dune-artdaq/Generators/WIBReader_generator.cc
+++ dune-artdaq/Generators/WIBReader_generator.cc
@@ -1,6 +1,7 @@
 #include "WIBReader.hh"
 #include "artdaq/Application/GeneratorMacros.hh"
 #include "dune-artdaq/DAQLogger/DAQLogger.hh"
+#include "cetlib/exception.h"
 
 #include <sstream>
 #include <vector>
@@ -19,21 +20,30 @@ namespace wibdaq {
 WIBReader::WIBReader(fhicl::ParameterSet const& ps) :
     CommandableFragmentGenerator(ps) {
 
-  std::string wib_address = ps.get<std::string>("WIB.address");
-  std::vector<std::vector<unsigned> > fakeColdDataModes(4,std::vector<unsigned>(2,1));
-  std::vector<std::vector<bool> > useWIBFakeData(4,std::vector<bool>(4,true));
-  std::vector<bool> enableDAQLink(4,true);
-  std::vector<bool> enableDAQRegs(4,0xF);
+  auto wib_address = ps.get<std::string>("WIB.address");
+  auto wib_table = ps.get<std::string>("WIB.wib_table");
+  auto femb_table = ps.get<std::string>("WIB.femb_table");
+  auto expected_wib_fw_version = ps.get<unsigned>("WIB.config.expected_wib_fw_version");
+  //auto expected_femb_fw_version = ps.get<unsigned>("WIB.config.expected_femb_fw_version");
+  auto expected_daq_mode = ps.get<std::string>("WIB.config.expected_daq_mode");
+  auto fake_cold_data_modes = ps.get<std::vector<std::vector<unsigned> > >("WIB.config.fake_cold_data_modes");
+  auto use_WIB_fake_data = ps.get<std::vector<std::vector<bool> > >("WIB.config.use_WIB_fake_data");
+  auto enable_DAQ_link = ps.get<std::vector<bool> >("WIB.config.enable_DAQ_link");
+  auto enable_DAQ_regs = ps.get<std::vector<unsigned> >("WIB.config.enable_DAQ_regs");
+  auto use_SI5342 = ps.get<bool>("WIB.config.use_SI5342");
 
   dune::DAQLogger::LogInfo("WIBReader") << "Connecting to WIB at " <<  wib_address;
-  wib = std::make_unique<WIB>( wib_address, "WIB.adt", "FEMB.adt" );
+  wib = std::make_unique<WIB>( wib_address, wib_table, femb_table );
 
   wib->ResetWIB();
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
+  // Check and print firmware version
+  uint32_t wib_fw_version = wib->Read("SYSTEM.FW_VERSION");
+
   dune::DAQLogger::LogInfo("WIBReader") << "WIB Firmware Version: 0x" 
         << std::hex << std::setw(8) << std::setfill('0')
-        <<  wib->Read("SYSTEM.FW_VERSION")
+        <<  wib_fw_version
         << " Synthesized: " 
         << std::hex << std::setw(2) << std::setfill('0')
         << wib->Read("SYSTEM.SYNTH_DATE.CENTURY")
@@ -50,6 +60,60 @@ WIBReader::WIBReader(fhicl::ParameterSet const& ps) :
         << std::hex << std::setw(2) << std::setfill('0')
         << wib->Read("SYSTEM.SYNTH_TIME.SECOND");
 
+  if (expected_wib_fw_version != wib_fw_version)
+  {
+    cet::exception excpt("WIBReader");
+    excpt << "WIB Firmware version is "
+        << std::hex << std::setw(8) << std::setfill('0')
+        << wib_fw_version
+        <<" but expect "
+        << std::hex << std::setw(8) << std::setfill('0')
+        << expected_wib_fw_version
+        <<" version in fcl";
+    throw excpt;
+  }
+
+  // Check if WIB firmware is for RCE or FELIX DAQ
+  WIB::WIB_DAQ_t daqMode = wib->GetDAQMode();
+
+  if (daqMode == WIB::RCE)
+  {
+    dune::DAQLogger::LogInfo("WIBReader") << "WIB Firmware setup for RCE DAQ Mode";
+    if(expected_daq_mode != "RCE" and expected_daq_mode != "rce")
+    {
+        cet::exception excpt("WIBReader");
+        excpt << "WIB Firmware setup in RCE mode, but expect '"<< expected_daq_mode <<"' mode in fcl";
+        throw excpt;
+    }
+  }
+  else if (daqMode == WIB::FELIX)
+  {
+    dune::DAQLogger::LogInfo("WIBReader") << "WIB Firmware setup for FELIX DAQ Mode";
+    if(expected_daq_mode != "FELIX" and expected_daq_mode != "felix")
+    {
+        cet::exception excpt("WIBReader");
+        excpt << "WIB Firmware setup in FELIX mode, but expect '"<< expected_daq_mode <<"' mode in fcl";
+        throw excpt;
+    }
+  }
+  else
+  {
+    cet::exception excpt("WIBReader");
+    excpt << "Unknown WIB firmware DAQ mode'"<< daqMode << "'";
+    throw excpt;
+  }
+
+  if(use_SI5342)
+  {
+    dune::DAQLogger::LogInfo("WIBReader") << "Configuring and selecting SI5342 for FELIX";
+    wib->LoadConfigDAQ_SI5342("default");
+    wib->SelectSI5342(1,true);
+  }
+  else
+  {
+    dune::DAQLogger::LogInfo("WIBReader") << "Not using SI5342 (don't need for RCE)";
+  }
+
   dune::DAQLogger::LogInfo("WIBReader") << "Setting up DTS";
 
   wib->InitializeDTS(1); // initDTS in script
@@ -61,23 +125,23 @@ WIBReader::WIBReader(fhicl::ParameterSet const& ps) :
   {
     for(uint8_t iCD=1; iCD <= 2; iCD++)
     {
-      wib->SetFEMBFakeCOLDATAMode(iFEMB,iCD,fakeColdDataModes.at(iFEMB-1).at(iCD-1)); // 0 for "SAMPLES" (counter) or 1 for "COUNTER" (nonsense)
+      wib->SetFEMBFakeCOLDATAMode(iFEMB,iCD,fake_cold_data_modes.at(iFEMB-1).at(iCD-1)); // 0 for "SAMPLES" (counter) or 1 for "COUNTER" (nonsense)
     }
     for(uint8_t iStream=1; iStream <= 4; iStream++)
     {
-      wib->SetFEMBStreamSource(iFEMB,iStream,!useWIBFakeData.at(iFEMB-1).at(iStream-1)); // last arg is bool isReal
+      wib->SetFEMBStreamSource(iFEMB,iStream,!use_WIB_fake_data.at(iFEMB-1).at(iStream-1)); // last arg is bool isReal
     }
     for(uint8_t iStream=1; iStream <= 4; iStream++)
     {
       uint64_t sourceByte = 0;
-      if(useWIBFakeData.at(iFEMB-1).at(iStream-1)) sourceByte = 0xF;
+      if(use_WIB_fake_data.at(iFEMB-1).at(iStream-1)) sourceByte = 0xF;
       wib->SourceFEMB(iFEMB,sourceByte);
     }
   }
 
   for(uint8_t iLink=1; iLink <= 4; iLink++)
   {
-    wib->EnableDAQLink_Lite(iLink,enableDAQLink.at(iLink-1));
+    wib->EnableDAQLink_Lite(iLink,enable_DAQ_link.at(iLink-1));
   }
 
   dune::DAQLogger::LogInfo("WIBReader") << "Enabling DTS";
@@ -95,7 +159,7 @@ WIBReader::WIBReader(fhicl::ParameterSet const& ps) :
   {
     std::stringstream regNameStream;
     regNameStream << "FEMB" << iFEMB << ".DAQ.ENABLE";
-    wib->Write(regNameStream.get(),enableDAQRegs.at(iFEMB-1));
+    wib->Write(regNameStream.get(),enable_DAQ_regs.at(iFEMB-1));
   }
 
   //fhicl::ParameterSet wib_config = ps.get<fhicl::ParameterSet>("WIB.config");
