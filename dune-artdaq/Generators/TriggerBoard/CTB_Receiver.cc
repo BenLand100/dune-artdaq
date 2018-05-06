9b9e5ec28a1fe8ec15b9be92a391f4dbe1272eaa
Marco Roda <mroda@liverpool.ac.uk>
2018-05-06 18:26:29 +0200

    Reading thread implemented, not tested

--- dune-artdaq/Generators/TriggerBoard/CTB_Receiver.cc
+++ dune-artdaq/Generators/TriggerBoard/CTB_Receiver.cc
@@ -1,2 +1,76 @@
 #include "CTB_Receiver.hh"
 
+CTB_Receiver::CTB_Receiver( const unsigned int port, const std::chrono::milliseconds & timeout ) : 
+  _port ( port ), _timeout( timeout ) {
+
+  _fut = std::async( launch::async, & CTB_Receiver::_receiver, this ) ;
+
+}
+
+CTB_Receiver::~CTB_Receiver() {
+
+  stop() ;
+
+}
+
+// access methods to the buffer
+//bool pop( ?? & u ) ;
+//bool pop( vector<??> & v ) ;
+
+
+bool CTB_Receiver::stop() {
+
+  _stop_requested = true ; 
+
+  int ret = _fut.get() ;
+
+}
+
+
+int CTB_Receiver::_receiver() {
+
+  boost::asio::io_service io_service;
+
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v6(), port ) );
+
+  tcp::socket socket(io_service);
+
+  std::future<void> accepting = async( launch::async, & acceptor::accept, acceptor, socket ) ;
+  
+  while ( ! _stop_requested ) {
+    
+    if ( accepting.wait_for( _timeout ) == future_status::ready ) 
+      break ;
+    // should this accept be async to avoid long block?
+      
+  }
+  
+  constexpr unsigned int max_size = 1024 ;
+  uint8_t tcp_data[max_size];
+
+  std::future<std::size_t> reading ; 
+  bool timed_out = false ;
+  
+  while ( ! _stop_requested ) {
+
+    if ( ! timed_out )  // no workng reading thread, need to create one
+      reading = async( launch::async, asio::read_some, socket,  boost::asio::buffer(tcp_data, max_size)  ) ;
+    
+    if ( ! accepting.wait_for( _timeout ) == future_status::ready ) {
+      timed_out = true ;
+      continue ;
+    }
+    
+    timed_out = false ;
+    const std::size_t read_bytes = reading.get() ;
+    
+    if ( read_bytes > 0 ) 
+      _buffer.push( tcp_data, read_bytes ) ;
+
+  }
+
+  // return because _stop_requested
+  return 0 ;
+}
+
+
--- dune-artdaq/Generators/TriggerBoard/CTB_Receiver.hh
+++ dune-artdaq/Generators/TriggerBoard/CTB_Receiver.hh
@@ -4,6 +4,7 @@
 #include <string>
 #include <atomic>
 #include <chrono>
+#include <future>
 
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
@@ -12,11 +13,25 @@
 #include "fhiclcpp/fwd.h"
 #include "dune_artdaq/dune-artdaq/Generators/TriggerBoard/content.h"
 
+/*
+
+  The CTB receiver is a a buffer in the form of a SPSC queue.
+  The _receiver method create the server and whenever data are received they are pushed in the buffer
+
+  The calling thread of the CTB_Receiver is supposed to pop these elements out
+  using pop
+
+
+*/ 
+
+
 class CTB_Receiver {
 
-  CTB_Receiver( unsigned int port, std::chrono::milliseconds timeout = 500 ) ;
+  CTB_Receiver( const unsigned int port, const std::chrono::milliseconds & timeout = 500 ) ;
   virtual ~CTB_Receiver() ;
 
+  // access methods to the buffer
+  // They should return words from the CTB receiver
   bool pop( ?? & u ) ;
   bool pop( vector<??> & v ) ;
   
@@ -24,11 +39,17 @@ class CTB_Receiver {
 
 private:
 
-  boost::lockfree::spsc_queue< ??? , boost::lockfree::capacity<1024> > _buffer ;  
+  boost::lockfree::spsc_queue< uint8_t , boost::lockfree::capacity<4096> > _buffer ;  
 
-  unsigned int port ;
+  // this is the receiver thread to be called
+  // return value to be used to 
+  int _receiver() ;   
+  std::future<int> _fut ;
 
-  std::chrono::milliseconds timeout = 500 ;
+
+  const unsigned int _port ;
+
+  std::chrono::milliseconds _timeout = 500 ;
   
   atomic<bool> _stop_requested = false ;
   atomic<bool> _timed_out = false ;
