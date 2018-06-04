0105b76bd14a98074696f176ca55d732a2c7ab1b
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2018-06-04 14:09:10 +0200

    Use non-blocking receive in FragmentSubscriber

--- dune-artdaq/Generators/timingBoard/FragmentSubscriber.cc
+++ dune-artdaq/Generators/timingBoard/FragmentSubscriber.cc
@@ -5,9 +5,13 @@
 
 #include <cassert>
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <vector>
 
 #include "stdint.h"
 
+// Are there more message parts waiting on the socket?
 bool rcvMore(void* socket)
 {
     int rcvmore;
@@ -16,42 +20,78 @@ bool rcvMore(void* socket)
     return rcvmore;
 }
 
-uint32_t getVal(void* socket, bool needMore)
+std::vector<uint32_t> getVals(void* socket)
 {
+    std::vector<uint32_t> ret;
     uint32_t val;
-    zmq_recv(socket, &val, sizeof(val), 0);
-    if(needMore) assert(rcvMore(socket));
-    return val;
+    int rc=zmq_recv(socket, &val, sizeof(val), ZMQ_DONTWAIT);
+    // Did we get anything on the socket?
+    if(rc==-1 && errno==EAGAIN){
+        // No, so return empty vector
+        return ret;
+    }
+    else if(rc==-1){
+        // Some other error. Print it and return empty vector
+        std::cout << "Error in zmq_recv(): " << strerror(errno) << std::endl;
+        return ret;
+    }
+    else{
+        // Yes, so receive the whole message
+        ret.push_back(val);
+        while(rcvMore(socket)){
+            zmq_recv(socket, &val, sizeof(val), ZMQ_RCVMORE);
+            ret.push_back(val);
+        }
+        return ret;
+    }
 }
 
 int main(int argc, char** argv)
 {
+    // connection_string should be compatible with the
+    // zmq_fragment_connection_out fcl parameter in the board reader
+    // (but with any "*" replaced with the machine name that the board
+    // reader is running on)
     if(argc!=2){
         std::cout << "Usage: FragmentSubscriber connection_string" << std::endl;
     }
 
+    // Create the zmq context and socket
     void* ctx = zmq_ctx_new();
     void* socket = zmq_socket(ctx, ZMQ_SUB);
+
+    // Connect the socket to the other end, and subscribe to all the messages on it
     zmq_connect(socket, argv[1]);
-    
     zmq_setsockopt(socket, ZMQ_SUBSCRIBE, "", 0);
-    
-    uint32_t cookie  = getVal(socket, true);
-    uint32_t scmd    = getVal(socket, true);
-    uint32_t tcmd    = getVal(socket, true);
-    uint32_t tstampl = getVal(socket, true);
-    uint32_t tstamph = getVal(socket, true);
-    uint32_t evtctr  = getVal(socket, true);
-    uint32_t cksum   = getVal(socket, false);
-    
-    std::cout << "Got fragment: " << std::endl
-              << "  cookie  = " << cookie  << std::endl
-              << "  scmd    = " << scmd    << std::endl
-              << "  tcmd    = " << tcmd    << std::endl
-              << "  tstampl = " << tstampl << std::endl
-              << "  tstamph = " << tstamph << std::endl
-              << "  evtctr  = " << evtctr  << std::endl
-              << "  cksum   = " << cksum   << std::endl;
+
+    // Loop for 10 seconds reading messages from the socket
+    auto startTime=std::chrono::system_clock::now();
+
+    while(true){
+        auto now=std::chrono::system_clock::now();
+        if(now-startTime > std::chrono::seconds(10)){
+            std::cout << "10 seconds up. Done" << std::endl;
+            break;
+        }
+
+        // Get the list of values from the socket. Returns empty vector if nothing available
+        std::vector<uint32_t> vals=getVals(socket);
+
+        if(vals.empty()){
+            // There was no message waiting.
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        }
+        else{
+            std::cout << "Got fragment: " << std::endl;
+            std::cout << "cookie :"   << vals[0] << std::endl;
+            std::cout << "scmd :"     << vals[1] << std::endl;
+            std::cout << "tcmd :"     << vals[2] << std::endl;
+            std::cout << "tstampl :"  << vals[3] << std::endl;
+            std::cout << "tstamph :"  << vals[4] << std::endl;
+            std::cout << "evtctr :"   << vals[5] << std::endl;
+            std::cout << "cksum :"    << vals[6] << std::endl;
+        }
+    }
 
     zmq_close(socket);
     zmq_ctx_destroy(ctx);
