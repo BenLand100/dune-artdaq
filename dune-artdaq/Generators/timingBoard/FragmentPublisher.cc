d01d81c3cf83cbc0fba93498c5c735a197773023
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2018-06-07 13:05:13 +0200

    Add fragment's sequence ID and fragment ID to the published fragment info, and make everything 64-bit

--- dune-artdaq/Generators/timingBoard/FragmentPublisher.cc
+++ dune-artdaq/Generators/timingBoard/FragmentPublisher.cc
@@ -23,22 +23,23 @@ int artdaq::FragmentPublisher::BindPublisher()
     return rc;
 }
 
-bool artdaq::FragmentPublisher::SendVal(uint32_t val, bool sendmore)
+bool artdaq::FragmentPublisher::SendVal(uint64_t val, bool sendmore)
 {
-    int rc = zmq_send(fZMQPublisherPtr, &val,  sizeof(uint32_t), sendmore ?  ZMQ_SNDMORE : 0);
+    int rc = zmq_send(fZMQPublisherPtr, &val,  sizeof(val), sendmore ?  ZMQ_SNDMORE : 0);
     return rc!=-1;
 }
 
-bool artdaq::FragmentPublisher::PublishFragment(dune::TimingFragment* frag)
+bool artdaq::FragmentPublisher::PublishFragment(artdaq::Fragment* frag, dune::TimingFragment* timingFrag)
 {
     bool success=true;
-    success = success && SendVal(frag->get_cookie(),  true);
-    success = success && SendVal(frag->get_scmd(),    true);
-    success = success && SendVal(frag->get_tcmd(),    true);
-    success = success && SendVal(frag->get_tstampl(), true);
-    success = success && SendVal(frag->get_tstamph(), true);
-    success = success && SendVal(frag->get_evtctr(),  true);
-    success = success && SendVal(frag->get_cksum(),   false);
+    success = success && SendVal(frag->sequenceID(),        true);
+    success = success && SendVal(frag->fragmentID(),        true);
+    success = success && SendVal(timingFrag->get_cookie(),  true);
+    success = success && SendVal(timingFrag->get_scmd(),    true);
+    success = success && SendVal(timingFrag->get_tcmd(),    true);
+    success = success && SendVal(timingFrag->get_tstamp(),  true);
+    success = success && SendVal(timingFrag->get_evtctr(),  true);
+    success = success && SendVal(timingFrag->get_cksum(),   false);
 
     return success;
 }
