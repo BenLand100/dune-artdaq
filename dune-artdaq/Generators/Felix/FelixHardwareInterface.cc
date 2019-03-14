7b4cf7b103fe31dca0335313102b7c41d773ef50
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2019-03-14 12:37:25 +0100

    Make it build

--- dune-artdaq/Generators/Felix/FelixHardwareInterface.cc
+++ dune-artdaq/Generators/Felix/FelixHardwareInterface.cc
@@ -126,7 +126,7 @@ FelixHardwareInterface::FelixHardwareInterface(fhicl::ParameterSet const& ps) :
     << "Setting up NetioHandler (host, port, adding channels, starting subscribers, locking subs to CPUs.)";
   nioh_.setupContext( backend_ ); // posix or infiniband
   for ( auto const & link : link_parameters_ ){ // Add channels
-    nioh_.addChannel(link.id_, link.tag_, link.host_, link.port_, queue_size_, zerocopy_); 
+    nioh_.addChannel(link.id_, link.tag_, link.host_, link.port_, queue_size_, zerocopy_, offset_-12, link.zmq_hit_send_connection_); 
   }
 
   DAQLogger::LogInfo("dune::FelixHardwareInterface::FelixHardwareInterface")
