462676d3a009d33c41e8eabeedfacfec5383f92d
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2019-06-11 12:41:22 +0200

    Add bin limits functions and some documentation

--- dune-artdaq/Generators/swTrigger/PowerTwoHist.hh
+++ dune-artdaq/Generators/swTrigger/PowerTwoHist.hh
@@ -4,27 +4,45 @@
 #include "stdint.h"
 #include <cstring> // For memset
 
-template<size_t N>
+// A simple histogram class that can only store unsigned integers in
+// bins of logarithmic size. Bin 0's limits are [0,0], bin 1 [1,1],
+// bin 2 [2,3], bin 3 [4, 7] and so on, up to bin N-1, which is an
+// overflow with limits [2**(N-1), UINT64_MAX]
+template<size_t Nbins>
 class PowerTwoHist
 {
 public:
     PowerTwoHist()
     {
         // Initialize all bin contents to zero
-        memset(bins, 0, N*sizeof(uint64_t));
+        memset(bins, 0, Nbins*sizeof(uint64_t));
     }
 
+    // Fill the bin corresponding to `x`. Return the filled bin number
+    // (might be overflow)
     size_t fill(unsigned long x)
     {
         int bin=64-__builtin_clzl(x);
-        if((size_t)bin>=N) bin=N-1;
+        if((size_t)bin>=Nbins) bin=Nbins-1;
         ++bins[bin];
         return bin;
     }
 
+    // Get the bin content in bin `ibin`
     uint64_t bin(size_t ibin) { return bins[ibin]; }
+
+    // The minimum value that will be stored in bin `ibin`
+    uint64_t binLo(size_t ibin) { return ibin==0 ? 0 : (1<<(ibin-1)); }
+    // The maximum value that will be stored in bin `ibin`
+    uint64_t binHi(size_t ibin)
+    {
+        if(ibin==0) return 0;
+        else if(ibin==Nbins-1) return UINT64_MAX;
+        else return (1<<ibin)-1;
+    }
+    
 private:
-    uint64_t bins[N];
+    uint64_t bins[Nbins];
 };
 
 #endif
--- dune-artdaq/Generators/swTrigger/test_PowerTwoHist.cxx
+++ dune-artdaq/Generators/swTrigger/test_PowerTwoHist.cxx
@@ -5,11 +5,15 @@
 int main(int, char**)
 {
     PowerTwoHist<10> hist;
+    hist.fill(0);
     hist.fill(1);
+    hist.fill(2);
     hist.fill(5);
+    hist.fill(100);
+    hist.fill(100);
     hist.fill(1<<12);
 
     for(size_t i=0; i<10; ++i){
-        std::cout << i << "\t" << hist.bin(i) << std::endl;
+        std::cout << i << "\t" << hist.binLo(i) << "\t" << hist.binHi(i) << "\t" << hist.bin(i) << std::endl;
     }
 }
