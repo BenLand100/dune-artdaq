6c9aca1b201f70f5ebb526b270d23b206d80d81b
Philip Rodrigues <philip.rodrigues@physics.ox.ac.uk>
2019-06-14 15:51:47 +0200

    Use nlohmann::json instead of jsoncpp, and add a function to return the ptmp socket config as a json object

--- dune-artdaq/Generators/swTrigger/ptmp_util.hh
+++ dune-artdaq/Generators/swTrigger/ptmp_util.hh
@@ -1,24 +1,31 @@
-#include "json/json.h"
+#include "json.hpp"
 
 namespace ptmp_util
 {
+    // `pattern` is the socket type (PUB, SUB, etc)
+    // `bind_or_connect` is a literal "bind" or "connect" for which end you want
+    // `endpoints` is a list of strings representing endpoints to bind/connect to, eg "tcp://*:12345"
+    nlohmann::json make_ptmp_socket_json(std::string pattern,
+                                      std::string bind_or_connect,
+                                      std::vector<std::string> endpoints)
+    {
+        using json = nlohmann::json;
+        json root;
+        root["socket"]["type"]=pattern;
+        for(auto const& ep: endpoints){
+            root["socket"][bind_or_connect].push_back(ep);
+        }
+        return root;
+    }
 
-// `pattern` is the socket type (PUB, SUB, etc)
-// `bind_or_connect` is a literal "bind" or "connect" for which end you want
-// `endpoints` is a list of strings representing endpoints to bind/connect to, eg "tcp://*:12345"
-std::string make_ptmp_socket_string(std::string pattern,
-                                    std::string bind_or_connect,
-                                    std::vector<std::string> endpoints)
-{
-    Json::Value root(Json::objectValue);
-    Json::Value endpoints_json(Json::arrayValue);
-    for(auto const& ep: endpoints) endpoints_json.append(ep);
-    root["socket"]=Json::Value(Json::objectValue);
-    root["socket"]["type"]=pattern;
-    root["socket"][bind_or_connect]=endpoints_json;
-    std::stringstream ss;
-    ss << root;
-    return ss.str();
-}
-
+    // `pattern` is the socket type (PUB, SUB, etc)
+    // `bind_or_connect` is a literal "bind" or "connect" for which end you want
+    // `endpoints` is a list of strings representing endpoints to bind/connect to, eg "tcp://*:12345"
+    std::string make_ptmp_socket_string(std::string pattern,
+                                        std::string bind_or_connect,
+                                        std::vector<std::string> endpoints)
+    {
+        return make_ptmp_socket_json(pattern, bind_or_connect, endpoints).dump();
+    }
+    
 }
