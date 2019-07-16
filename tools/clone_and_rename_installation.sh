93d5119b41779097d17708c9cce50beeedb6f0be
John Christian Freeman <jofreema@np04-srv-012.cern.ch>
2019-07-16 18:57:20 +0200

    JCF: clone_and_rename_installation.sh can work with local build areas
    It remains the case that clone_and_rename_installation.sh should only be
    used with expert's permission, and only when needed - e.g., if there's a
    problem with the network s.t. it's not possible to pull code from
    Fermilab's central repository. However, should it ever be needed, it's
    now the case that if the script determines that it's cloning a locally-built
    dune-artdaq installation - i.e., one that resides on /scratch rather than
    /nfs/sw/work_dirs - it will be able to handle this. I've successfully tested
    dune-artdaq development areas created with this script (commenting out the exit
    at the top) using run 8887 (a cloned local-build installation) and run 8888 (a
    cloned, traditional-style NFS installation)

--- tools/clone_and_rename_installation.sh
+++ tools/clone_and_rename_installation.sh
@@ -5,6 +5,7 @@ if [[ "$#" != "2"  ]]; then
     exit 1
 fi
 
+
 cat<<EOF
 
 JCF, Jun-6-2019
@@ -28,6 +29,7 @@ EOF
 
 exit 1
 
+
 orig_installation=$1
 new_installation=$2
 
@@ -47,15 +49,61 @@ if [[ -e $new_installation ]]; then
     exit 3
 fi
 
-if [[ ! $new_installation =~ ^/nfs/sw/work_dirs ]]; then
-    echo "Requested new installation area $new_installation should be located in /nfs/sw/work_dirs; exiting..." >&2
-    exit 4
-fi
-
 if [[ ! -e $orig_installation/setupDUNEARTDAQ_forBuilding ]]; then
     echo "Unable to find expected file $orig_installation/setupDUNEARTDAQ_forBuilding in original dune-artdaq installation $orig_installation; exiting..." >&2
     exit 5
     
+fi
+
+nfs_base=/nfs/sw/work_dirs
+local_base=/scratch
+
+if [[ "$orig_installation" =~ ^$nfs_base ]]; then
+    is_nfs_area=true
+elif [[ "$orig_installation" =~ ^$local_base ]]; then
+    is_nfs_area=false
+else
+
+    cat<<EOF >&2
+
+This script can't clone dune-artdaq development areas that aren't
+either in a subdirectory of $nfs_base or $local_base. Exiting...
+
+EOF
+    exit 1
+fi
+
+if ! $is_nfs_area ; then
+    orig_installation_for_running=$( echo $orig_installation | sed -r 's#'$local_base'#'$nfs_base'#' )
+    new_installation_for_running=$( echo $new_installation | sed -r 's#'$local_base'#'$nfs_base'#' )
+
+    if [[ -e $orig_installation_for_running ]]; then
+	mkdir $new_installation_for_running
+
+	if [[ "$?" != 0 ]]; then
+	    echo "There was a problem attempting to make directory $new_installation_for_running (perhaps it already existed?); exiting..." >&2
+	    exit 1
+	fi
+
+	for script in setupDUNEARTDAQ_forTRACE setupDUNEARTDAQ_forInhibitMaster setupDUNEARTDAQ_forRunning; do
+
+	    if [[ -e $orig_installation_for_running/$script ]]; then
+		cp $orig_installation_for_running/$script $new_installation_for_running
+	    else
+		echo "Unable to find expected script $orig_installation_for_running/$script, exiting..." >&2
+		exit 1
+	    fi
+	done
+    else
+	cat<<EOF
+
+Expected there to be a directory on the NFS disk,
+$orig_installation_for_running, used for running the code built in
+$orig_installation. No such directory was found. Exiting...
+
+EOF
+	exit 1
+    fi
 fi
 
 num_local_products_dirs=$( find $orig_installation -maxdepth 1 -type d -regex ".*localProducts.*prof\|debug" | wc -l )
@@ -74,7 +122,7 @@ if (( $num_local_products_dirs == 1 )); then
     echo "Build type: $buildtype"
 
 else
-    echo "Expected to find 1 local products directory in ${orig_installation}; found $num_local_products_dirs instead. Exiting..." >&2
+    echo "Expected to find one local products directory in ${orig_installation}; found $num_local_products_dirs instead. Exiting..." >&2
     exit 5
 fi
 
@@ -94,11 +142,27 @@ echo "Copy took "$(( endtime - begintime ))" seconds; will now attempt to rebuil
 
 cd $new_installation
 
-orig_installation_subdir=$( basename $orig_installation )
-new_installation_subdir=$( basename $new_installation )
-sed -r -i 's#trace_'$orig_installation_subdir'#trace_'$new_installation_subdir'#g' $new_installation/setupDUNEARTDAQ*
+if $is_nfs_area; then
+orig_installation_subdirs=$( echo $orig_installation | sed -r 's#'$nfs_base'/(.*)#\1#' )
+new_installation_subdirs=$( echo $new_installation | sed -r 's#'$nfs_base'/(.*)#\1#' )
+else
+orig_installation_subdirs=$( echo $orig_installation | sed -r 's#'$local_base'/(.*)#\1#' )
+new_installation_subdirs=$( echo $new_installation | sed -r 's#'$local_base'/(.*)#\1#' )
+fi
 
-sed -r -i 's#'$orig_installation'/#'$new_installation'/#g' $new_installation/setupDUNEARTDAQ*
+if $is_nfs_area; then
+    scripts_to_edit="$new_installation/setupDUNEARTDAQ_forBuilding $new_installation/setupDUNEARTDAQ_forRunning  $new_installation/setupDUNEARTDAQ_forTRACE  $new_installation/setupDUNEARTDAQ_forInhibitMaster"
+else
+    scripts_to_edit="$new_installation/setupDUNEARTDAQ_forBuilding $new_installation_for_running/setupDUNEARTDAQ_forRunning  $new_installation_for_running/setupDUNEARTDAQ_forTRACE  $new_installation_for_running/setupDUNEARTDAQ_forInhibitMaster"
+fi
+
+
+for script in $scripts_to_edit; do
+    echo     sed -r -i 's#trace_.*#trace_'$( echo $new_installation_subdirs | sed -r 's#^/##;s#/#_#g')'#' $script
+    echo     sed -r -i 's#'$orig_installation_subdirs'/#'$new_installation_subdirs'/#g' $script
+    sed -r -i 's#trace_.*#trace_'$( echo $new_installation_subdirs | sed -r 's#^/##;s#/#_#g')'#' $script
+    sed -r -i 's#'$orig_installation_subdirs'/#'$new_installation_subdirs'/#g' $script
+done
 
 rm -rf $new_installation/localProducts_*
 rm -rf $new_installation/build_slf7.x86_64
@@ -126,20 +190,21 @@ fi
 no_mrb_script=localProducts_${project}_${version}_${qualifiers}_${buildtype}/setupWithoutMRB
 
 if [[ -e $orig_installation/$no_mrb_script ]]; then
-    cp $orig_installation/$no_mrb_script $new_installation/$no_mrb_script
+    if $is_nfs_area; then
+	cp $orig_installation/$no_mrb_script $new_installation/$no_mrb_script
+    else
+	cp -rp $PWD/localProducts_${project}_${version}_${qualifiers}_${buildtype} $new_installation_for_running
+	cp $orig_installation/$no_mrb_script $new_installation_for_running/$no_mrb_script
+    fi
 fi
 
+source /nfs/sw/artdaq/products/setup                                      
+source /nfs/sw/artdaq/products_dev/setup    
 source $PWD/localProducts_${project}_${version}_${qualifiers}_${buildtype}/setup
 
 if [[ "$?" != "0" ]]; then
     echo "There was a problem source-ing $PWD/localProducts_${project}_${version}_${qualifiers}_${buildtype}/setup" >&2
     exit 8
-fi
-
-# Can remove this as soon as I figure out how to copy soflinks correctly when doing a recursive directory copy...
-if [[ -e setupDUNEARTDAQ_forRunning ]]; then
-    rm -f setupDUNEARTDAQ
-    ln -s setupDUNEARTDAQ_forRunning setupDUNEARTDAQ 
 fi
 
 echo "About to call mrb z...."
@@ -153,4 +218,13 @@ echo "FAILURE ABOVE IS EXPECTED; WILL PERFORM REBUILD ATFER SOURCE OF setupDUNEA
 . setupDUNEARTDAQ_forBuilding
 mrb install -j$processors
 
+
+# Can remove this as soon as I figure out how to copy softlinks correctly when doing a recursive directory copy...
+if $is_nfs_area; then
+    rm -f $new_installation/setupDUNEARTDAQ
+    ln -s $new_installation/setupDUNEARTDAQ_forRunning $new_installation/setupDUNEARTDAQ 
+else
+    rm -f $new_installation_for_running/setupDUNEARTDAQ
+    ln -s $new_installation_for_running/setupDUNEARTDAQ_forRunning $new_installation_for_running/setupDUNEARTDAQ
+fi
 
