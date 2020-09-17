55a78c282fcbcc00e332e1727f12ff93b5c1aa4f
Kurt Biery <biery@fnal.gov>
2020-09-17 15:22:44 -0500

    JCF: (may not compile) get timingBoard/ subdirectory identical to what it was during successful runs with timing fragment generator at iceberg (e.g., run 5108)

--- dune-artdaq/Generators/timingBoard/TimingMain.cc
+++ dune-artdaq/Generators/timingBoard/TimingMain.cc
@@ -43,6 +43,10 @@
 
 #include "messagefacility/MessageLogger/MessageLogger.h"
 
+#include "StatusPublisher.hh"
+#include "FragmentPublisher.hh"
+#include "HwClockPublisher.hh"
+
 // Fiddle (static global: There should be a nicer way of allowing the 
 // data_take() function to see the trg object)
 dune::TimingReceiver* trgp = 0;
@@ -51,7 +55,7 @@ dune::TimingReceiver* trgp = 0;
 
 void data_take(int) {     // Main program of data taking thread
   printf("data_take: Started thread for GetNext\n");
-  artdaq::FragmentPtrs frags;
+  artdaq::PostmarkedFragmentPtrs frags;
 
   int seen_stop = 0;
   int nemptycalls = 0;
@@ -87,13 +91,16 @@ int main(int argn, char** argv) {
 
   int partition=0;
   int millisecwait = 30000;
-
-  if (argn > 3) {
-    printf("usage: %s [partition] [millisec time (default 30s)]\n",argv[0]);
+  int enable_spill_commands_int = 1;
+  if (argn > 4) {
+    printf("usage: %s [partition] [millisec time (default 30s)] [enable spill commands]\n",argv[0]);
     return 1;
   }
   if(argn>=2)  sscanf(argv[1],"%d",&partition);
-  if(argn==3)  sscanf(argv[2],"%d",&millisecwait);
+  if(argn>=3)  sscanf(argv[2],"%d",&millisecwait);
+  if(argn>=4)  sscanf(argv[3],"%d",&enable_spill_commands_int);
+
+  bool enable_spill_commands=enable_spill_commands_int;
 
   mf::setStandAloneMessageThreshold({"DEBUG"});
 
@@ -106,9 +113,13 @@ int main(int argn, char** argv) {
   ps.put<int>("fragment_id", 5);
   ps.put<int>("debug_print", 3);
   ps.put<int>("partition_number", partition);
-  ps.put<std::string>("connections_file", "/nfs/sw/timing/dev/software/v4a3/timing-board-software/tests/etc/connections.xml");
-  ps.put<std::string>("hardware_select", "PDTS_TERTIARY");
+  ps.put<bool>("enable_spill_commands", enable_spill_commands);
+  std::vector<int> firmware_versions{0x050100};
+  ps.put<std::vector<int>>("valid_firmware_versions", firmware_versions);
+  ps.put<std::string>("connections_file", "/nfs/sw/control_files/timing/connections_v5a1.xml");
+  ps.put<std::string>("hardware_select", "PROD_FANOUT_1");
   ps.put<std::string>("zmq_connection", "tcp://localhost:5601");
+  ps.put<std::string>("run_trigger_output", "/tmp/");
   dune::TimingReceiver trg(ps);       // (1) This does the configure step
   trgp = &trg;                        // Set this global variable to complete the fiddle at the start
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
