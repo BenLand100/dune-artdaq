8bfaafdf5a9b1b6182bbc6d0f458b4f7ca92368b
Benjamin Land <benland100@gmail.com>
2021-03-19 13:21:51 -0500

    Add synchronous, triggered WIB2 spy buffer readout to WIB2Reader

--- dune-artdaq/Generators/WIB2Reader.hh
+++ dune-artdaq/Generators/WIB2Reader.hh
@@ -6,6 +6,8 @@
 #include "artdaq-core/Data/Fragment.hh"
 #include "artdaq/Generators/CommandableFragmentGenerator.hh"
 
+#include "dune-artdaq/Generators/wib2/wib.pb.h"
+#include "Felix/RequestReceiver.hh"
 
 namespace wib2daq {
 
@@ -24,7 +26,11 @@ private:
   void start() override;
   // "stop" transition
   void stop() override;
+  
+  bool acquireAsyncData(artdaq::FragmentPtrs& frags);
+  void acquireSyncData(artdaq::FragmentPtrs& frags);
   bool getNext_(artdaq::FragmentPtrs& output) override;
+  
   void stopNoMutex() override {}
 
   void setupWIB(const fhicl::ParameterSet &WIB_config);
@@ -33,7 +39,15 @@ private:
   void send_command(const C &msg, R &repl); 
   
   bool run_script(std::string name);
-
+  
+  uint8_t trigger_command = 0; //TLU command code
+  uint32_t trigger_rec_ticks = 0; //4.15834 ns ticks (18 bit max)
+  uint32_t trigger_timeout_ms = 0; //Maximum number of ms to wait for trigger (32 bit max)
+  std::unique_ptr<RequestReceiver> request_receiver = nullptr;
+  std::unique_ptr<TriggerInfo> last_trigger = nullptr;
+  std::unique_ptr<wib::ReadDaqSpy::DaqSpy> last_data = nullptr;
+  
+  
   zmq::context_t *context = NULL;
   zmq::socket_t *socket = NULL;
   bool spy_buffer_readout = false;
